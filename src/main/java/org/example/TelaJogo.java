7786da1d46f924a776d2efad06f4dcef88257a97
Helen Silva <helen_silva32@hotmail.com>
2025-03-10 14:32:32 -0300

    creating methods: TelaJogo + iniciarJogo + desenharTela + fimDeJogo + criarBloco

--- src/main/java/org/example/TelaJogo.java
+++ src/main/java/org/example/TelaJogo.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
@@ -25,6 +26,66 @@ public class TelaJogo extends JPanel implements ActionListener {
     Timer timer;
     Random random;
 
+    TelaJogo() {
+        random = new Random();
+        setPreferredSize(new Dimension(LARGURA_TELA, ALTURA_TELA));
+        setBackground(Color.white);
+        setFocusable(true);
+        //addKeyListener(new LeitorDeTeclasAdapter());
+        iniciarJogo();
+    }
+
+    public void iniciarJogo() {
+        criarBloco();
+        estaRodando = true;
+        timer = new Timer(INTERVALO, this);
+        timer.start();
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        desenharTela(g);
+    }
+
+    public void desenharTela(Graphics g){
+        if(estaRodando) {
+            g.setColor(Color.red);
+            g.fillOval(blocoX, blocoY, TAMANHO_BLOCO, TAMANHO_BLOCO); // bloco a ser comido
+
+            for (int i = 0; i < corpoCobra; i++) {
+                if (i == 0) { // cabeça
+                    g.setColor(Color.green);
+                    g.fillRect(eixoX[0], eixoY[0], TAMANHO_BLOCO, TAMANHO_BLOCO);
+                } else { // corpo
+                    g.setColor(new Color(45, 180, 0));
+                    g.fillRect(eixoX[0], eixoY[0], TAMANHO_BLOCO, TAMANHO_BLOCO);
+                }
+            }
+            g.setColor(Color.red);
+            g.setFont(new Font(NOME_FONTE, Font.BOLD, 40));
+            FontMetrics metrics = getFontMetrics(g.getFont());
+            g.drawString("Pontos: " + blocosComidos, (LARGURA_TELA - metrics.stringWidth("Pontos: " + blocosComidos)) / 2, g.getFont().getSize());
+        } else {
+            fimDeJogo(g);
+        }
+    }
+
+    public void fimDeJogo(Graphics g){
+        g.setColor(Color.red);
+        g.setFont(new Font(NOME_FONTE, Font.BOLD, 40));
+        FontMetrics fontePontuacao = getFontMetrics(g.getFont());
+        g.drawString("Pontos: " + blocosComidos, (LARGURA_TELA - fontePontuacao.stringWidth("Pontos: " + blocosComidos)) / 2, g.getFont().getSize());
+        g.setColor(Color.red);
+        g.setFont(new Font(NOME_FONTE, Font.BOLD, 75));
+        FontMetrics fonteFinal = getFontMetrics(g.getFont());
+        g.drawString("\uD83D\uDE10 Fim do Jogo", (LARGURA_TELA - fonteFinal.stringWidth("Fim do Jogo")) / 2, ALTURA_TELA / 2);
+    }
+
+    public void criarBloco() {
+        blocoX = random.nextInt(LARGURA_TELA / TAMANHO_BLOCO) * TAMANHO_BLOCO;
+        blocoY = random.nextInt(ALTURA_TELA / TAMANHO_BLOCO) * TAMANHO_BLOCO;
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
 
